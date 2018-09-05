cdf65760a1c1995fce2aed64a044eacfa409f73f
lbeckman314 <lbeckman314@gmail.com>
2018-09-05 13:26:24 -0700

    updated dbcon.js

--- .build.sh
+++ .build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 project=biblioteca
-production=/var/www/pkgs/$project
+pkgs=/var/www/pkgs/$project
+production=/var/www/$project
 
+# restart production server
+
+#ps -aux | grep --extended-regexp "[8]083" | awk '{print $2}'
+cd $production
+./node_modules/forever/bin/forever stop database.js
+git pull origin master
+./node_modules/forever/bin/forever database.js 8083
+
+
+# move compressed files and sha/gpg signatures to packages directory
 git archive --format=tar -v -o $project.tar.gz HEAD
 git archive --format=zip -v -o $project.zip HEAD
-
-#tar -zcvf $project.tar.gz $project
-#zip -r $project.zip $project
 
 sha256sum *.tar.gz *.zip > sha256sums.txt
 gpg --pinentry-mode loopback --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
 
-mv $project.tar.gz $project.zip sha256sums.txt* $production
+mv $project.tar.gz $project.zip sha256sums.txt* $pkgs
