198d4ea4c5d623471918bbd36494f1a06c4a4297
lbeckman314 <lbeckman314@gmail.com>
2018-09-05 13:00:18 -0700

    moved dbcon.js to dbcon-example.ls

--- dbcon-example.js
+++ dbcon-example.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var pool = mysql.createConnection({
   connectionLimit : 10,
   host            : 'localhost',
-  user            : 'cs340_beckmanl',
-  password        : '0734',
-  database        : 'cs340_beckmanl',
+  user            : 'USERNAME',
+  password        : 'PASSWORD',
+  database        : 'DATABASE',
     multipleStatements: true
 });
 
